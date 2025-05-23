ad4c0f50c5d99efb387fcfa86127938900cb79fc
Luciuus <infelxeon@gmail.com>
2025-05-23 21:39:58 +0700

    Refactor Home component: add a blank line for improved code readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [musicVolume, setMusicVolume] = useState(0.15);
 
   const audioRef = useRef<HTMLAudioElement | null>(null); // holds the same audio object
+  
 
   useEffect(() => {
     const timer = setTimeout(() => setFadeOut(true), 1500);
